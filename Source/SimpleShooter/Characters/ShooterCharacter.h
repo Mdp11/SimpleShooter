1c94dd0deb4f27e6ea242c54b3ad06e68437a8f1
Mattia De Prisco <mattia.dipi@gmail.com>
2020-11-13 23:47:12 +0100

    Setup base movement

--- Source/SimpleShooter/Characters/ShooterCharacter.h
+++ Source/SimpleShooter/Characters/ShooterCharacter.h
@@ -9,21 +9,25 @@
 UCLASS()
 class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	AShooterCharacter();
+    // Sets default values for this character's properties
+    AShooterCharacter();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(
+        class UInputComponent* PlayerInputComponent) override;
 
+private:
+    void MoveForward(const float AxisValue);
+    void MoveRight(const float AxisValue);
 };
